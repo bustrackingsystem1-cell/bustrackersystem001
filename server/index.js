1c853bad51664edd3322087ea362bce629e7e89e
bustrackingsystem29-byte <bustrackingsystem29@gmail.com>
2025-09-25 20:34:25 +0530

    Fix ES module imports in server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const os = require('os');
+import express from 'express';
+import cors from 'cors';
+import os from 'os';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
